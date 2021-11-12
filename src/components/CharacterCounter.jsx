953e9b5418c1ec874e55d66f536dbce63baa60dd
Kevin Shreve <kshreve@wayup.com>
2021-11-12 15:44:03 -0500

    useRecoilValue

---
+++ src/components/CharacterCounter.jsx
@@ -0,0 +1,13 @@
+import TextInput from './TextInput'
+import CharacterCount from './CharacterCount'
+
+function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
+
+export default CharacterCounter;
