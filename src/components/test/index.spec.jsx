c89c863969aff71bac7264fbda0c0b801830a2e6
Kevin Shreve <kshreve@wayup.com>
2021-11-16 16:27:44 -0500

    add test

---
+++ src/components/test/index.spec.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { RecoilRoot, useRecoilValue } from 'recoil';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import Form, { nameAtom } from './index';
+
+export const RecoilObserver = ({ node, onChange }) => {
+  const value = useRecoilValue(node);
+  useEffect(() => onChange(value), [onChange, value]);
+  return null;
+};
+
+describe('The form state should', () => {
+  test('change when the user enters a name.', () => {
+    const onChange = jest.fn();
+
+    render(
+      <RecoilRoot>
+        <RecoilObserver node={nameAtom} onChange={onChange} />
+        <Form />
+      </RecoilRoot>
+    );
+
+    const component = screen.getByTestId('name_input');
+
+    fireEvent.change(component, { target: { value: 'Recoil' } });
+
+    expect(onChange).toHaveBeenCalledTimes(2);
+    expect(onChange).toHaveBeenCalledWith(''); // Initial state on render.
+    expect(onChange).toHaveBeenCalledWith('Recoil'); // New value on change.
+  });
+});
